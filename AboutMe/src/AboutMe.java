d08359bf13ec140190b10edb00de2068b146c015
thePaulCode <santospaulor21@gmail.com>
2023-08-27 21:53:46 -0300

    Tratamento de excecoes

--- AboutMe/src/AboutMe.java
+++ AboutMe/src/AboutMe.java
@@ -27,12 +27,14 @@ public class AboutMe {
  * para ter uma melhor experiência ao executar o nosso programa
  */
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
         //criando o objeto scanner
+        try {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
         System.out.println("Digite seu nome");
@@ -52,6 +54,12 @@ public class AboutMe {
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Os campos idade e altura precisam ser numéricos");
+
+        }
         
         
     }
